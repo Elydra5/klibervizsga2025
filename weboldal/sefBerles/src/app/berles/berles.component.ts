beeeb6f477d70ec40030edf290834fdbca6bd93f
Klíber Benedek <kliberbenedek@gmail.com>
2025-05-30 16:57:28 +0200

    angular minifix

--- weboldal/sefBerles/src/app/berles/berles.component.ts
+++ weboldal/sefBerles/src/app/berles/berles.component.ts
@@ -8,36 +8,43 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './berles.component.css'
 })
 export class BerlesComponent {
-  selectedChef: any;
-  startDate: string = '';
-  endDate: string = '';
+  selectedChef: any
+  startDate: string = ''
+  endDate: string = ''
+  successMessage: string = ''
+  errorMessage: string = ''
 
-  firebaseUrl = 'https://p161-7ddfd-default-rtdb.europe-west1.firebasedatabase.app/chefs.json';
-  backendUrl = 'http://localhost:3000/api/berlesek'
+  firebaseUrl = 'https://p161-7ddfd-default-rtdb.europe-west1.firebasedatabase.app/chefs.json'
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.route.params.subscribe(params => {
-      const chefId = params['id'];
-      this.getChefById(chefId);
-    });
+      const chefId = params['id']
+      this.getChefById(chefId)
+    })
   }
 
   getChefById(id: string) {
     this.http.get<{ [key: string]: any }>(this.firebaseUrl).subscribe(data => {
       const chefs = Object.values(data)
-      this.selectedChef = chefs.find((chef: any) => chef.id == id);
-    });
+      this.selectedChef = chefs.find((chef: any) => chef.id == id)
+    })
   }
 
   submitBerles() {
-    const berlesData = {
-      userId: 101,
-      chefId: this.selectedChef.id,
-      from: this.startDate,
-      to: this.endDate
+  const berlesData = {
+    userId: 101,
+    chefId: this.selectedChef.id,
+    from: this.startDate,
+    to: this.endDate
+  };
+
+  this.http.post('http://localhost:3000/api/berlesek', berlesData).subscribe({
+    next: () => {
+      this.successMessage = 'A bérlés sikeresen rögzítve lett!';
+    },
+    error: (err) => {
+      this.errorMessage = 'Hiba történt a bérlés rögzítése során: ' + err.message;
     }
-    this.http.post(this.backendUrl, berlesData).subscribe(() => {
-      
-    })
-  }
+  })
+}
 }
